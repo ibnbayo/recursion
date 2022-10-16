a6070973d607d82b728fccca4c5b4766dac84238
AF <100952220+ibnbayo@users.noreply.github.com>
2022-10-16 10:30:55 +0100

    spelling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className=' grid-rows-3'>
         <div>
         <h1 className='text-6xl font-extrabold justify-center'>Teaching has never been easier </h1>
-        <p>Host live classes, collaborate with learing mates and manage your tutors/students</p>
+        <p>Host live classes, collaborate with learning mates and manage your tutors/students</p>
         </div>
         <img src='https://cdn.nerdschalk.com/wp-content/uploads/2020/05/nerdschalk.com-3x3-video-view-on-microsoft-teams.jpg?width=1300' alt='video-app'/>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
